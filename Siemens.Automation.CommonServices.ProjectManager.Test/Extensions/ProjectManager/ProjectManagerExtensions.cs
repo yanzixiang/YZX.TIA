7896915ca31e966e60268c39ab3f99ad70da9d0f
yanzixiang <whyzix@qq.com>
2021-11-17 17:20:09 +0800

    add Siemens.Automation.CommonServices.ProjectManager.Test

---
+++ Siemens.Automation.CommonServices.ProjectManager.Test/Extensions/ProjectManager/ProjectManagerExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Siemens.Automation.Basics;
+using Siemens.Automation.CommonServices;
+using Siemens.Automation.ProjectManager.Impl.Tia;
+
+using YZX.Tia.Proxies.ProjectManager;
+
+namespace YZX.Tia.Extensions.ProjectManager
+{
+  public static class ProjectManagerExtensions
+  {
+    public static TiaProjectManagerLegacyHandlerProxy GetProjectManagerProxy(this IWorkingContext workingContext)
+    {
+      return new TiaProjectManagerLegacyHandlerProxy(GetProjectManager(workingContext));
+    }
+
+    public static IProjectManager GetProjectManager(this IWorkingContext workingContext)
+    {
+      return workingContext.GetRequiredDlc<IProjectManager>("Siemens.Automation.CommonServices.ProjectManager");
+    }
+
+    internal static TiaProjectManager GetTiaProjectManager(this IWorkingContext workingContext)
+    {
+      ITiaProjectActionerFactory requiredDlc = workingContext.GetRequiredDlc<ITiaProjectActionerFactory>( 
+        "Siemens.Automation.ProjectManager.Impl.Tia.TiaProjectActionerFactory");
+      return new TiaProjectManager(workingContext, requiredDlc);
+    }
+
+    public static TiaProjectManagerProxy GetTiaProjectManagerProxy(this IWorkingContext workingContext)
+    {
+      TiaProjectManager tiaProjectManager = workingContext.GetTiaProjectManager();
+      TiaProjectManagerProxy tiaProjectManagerProxy = new TiaProjectManagerProxy(tiaProjectManager);
+      return tiaProjectManagerProxy;
+    }
+  }
+}
---
+++ Siemens.Automation.CommonServices.ProjectManager.Test/Proxies/ProjectManager/TiaProjectProxy.cs
@@ -0,0 +1,154 @@
+﻿using System;
+
+using Siemens.Automation.Basics;
+using Siemens.Automation.ObjectFrame;
+using Siemens.Automation.ProjectManager;
+using Siemens.Automation.FrameApplication;
+using Siemens.Automation.ProjectManager.Impl.Tia;
+
+#if Tia
+using Siemens.Simatic.PlcLanguages.BlockLogic;
+using Siemens.Simatic.PlcLanguages.VatService;
+using Siemens.Simatic.PlcLanguages.BlockLogic.PLDebug;
+#endif
+
+using Siemens.Simatic.Hmi.Utah.Common.Base.Reflection;
+
+using YZX.Tia.Dlc;
+using YZX.Tia.Proxies.BlockLogic;
+
+namespace YZX.Tia.Proxies.ProjectManager
+{
+  public partial class TiaProjectProxy
+  {
+    TiaProject tp;
+
+    ITiaProject TiaProject
+    {
+      get
+      {
+        return tp as ITiaProject;
+      }
+    }
+    ITiaProjectInternal TiaProjectInternal
+    {
+      get
+      {
+        return tp as ITiaProjectInternal;
+      }
+    }
+
+    public TiaProjectProxy(object o)
+    {
+      tp = o as TiaProject;
+    }
+
+    public IWorkingContext WorkingContext
+    {
+      get
+      {
+        return TiaProject.WorkingContext;
+      }
+    }
+
+    public IWorkingContext ProjectWorkingContext
+    {
+      get
+      {
+        ICoreContext projectContext = WorkingContext.GetCoreContext();
+        return ((IDlc)projectContext).WorkingContext;
+      }
+    }
+
+    
+
+    public void UnlockStarterFile()
+    {
+      TiaProjectInternal.UnlockStarterFile();
+    }
+
+    public string DirectoryPath
+    {
+      get
+      {
+        return (string)Reflector.GetInstancePropertyByName(tp,
+          "DirectoryPath",
+          ReflectionWays.SystemReflection);
+      }
+    }
+
+    public string StarterFilePath
+    {
+      get
+      {
+        return (string)Reflector.GetInstancePropertyByName(tp,
+          "StarterFilePath",
+          ReflectionWays.SystemReflection);
+      }
+    }
+
+    public event EventHandler MainWorkingContextIsClosing
+    {
+      add
+      {
+        tp.MainWorkingContextIsClosing += value;
+      }
+      remove
+      {
+        tp.MainWorkingContextIsClosing -= value;
+      }
+    }
+
+    public event EventHandler IsModifiedChanged
+    {
+      add
+      {
+        tp.IsModifiedChanged += value;
+      }
+      remove
+      {
+        tp.IsModifiedChanged -= value;
+      }
+    }
+
+#if Tia
+    public QueryBasedControllerTargetLookupProxy QueryBasedControllerTargetLookup
+    {
+      get
+      {
+        var coreContext = WorkingContext.GetCoreContext();
+        var QueryBasedControllerTargetLookupProxy = 
+          new QueryBasedControllerTargetLookupProxy(coreContext);
+        return QueryBasedControllerTargetLookupProxy;
+      }
+    }
+
+    public ICoreObject FindCPU(string name)
+    {
+      return QueryBasedControllerTargetLookup.FindControllerTargetByName(name);
+    }
+
+    public ILibraryVersionService LibraryVersionService
+    {
+      get
+      {
+        return ProjectWorkingContext.DlcManager.Load("Siemens.Simatic.PlcLanguages.BlockLogic.LibraryVersionService") as ILibraryVersionService;
+      }
+    }
+#endif
+
+    public void Close(bool Silent = true)
+    {
+      var cpp = new CloseProjectParams();
+      cpp.Silent = Silent;
+      TiaProject.Close(cpp);
+    }
+
+    public TiaProjectState State {
+      get
+      {
+        return TiaProject.State;
+      }
+    }
+  }
+}
